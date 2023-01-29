e974022e8fe20444186ae15b332c035240e3df85
mauroSantulli <108669591+Mauro-Santulli@users.noreply.github.com>
2023-01-29 11:29:00 +0100

    Add Create a resource

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+const port = 3000;
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get(`/`, async (req: Request, res: Response) => {
+    const vGames = await prisma.videogame.findMany();
+    res.json(vGames);
+});
+
+
+// creazione record prisma
+app.post(`/videogame`, async (req: Request, res: Response) => {
+    console.log("post");
+    const { nome, genere } = req.body;
+    const vGames = await prisma.videogame.create({
+        data: {
+            nome: nome,
+            genere: genere
+        }
+    })
+    res.json(vGames);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
